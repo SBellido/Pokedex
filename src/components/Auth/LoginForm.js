67001516fcfcffdfc54c1b94d543ef259d1a1d52
SBellido <sebastianbellidodg@gmail.com>
2024-05-03 18:43:32 -0300

    Username and Password is working in local context

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   TextInput,
@@ -7,8 +7,30 @@ import {
   View,
   Keyboard,
 } from "react-native";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { user, userDetails } from "../../utils/userDB";
 
 export default function LoginForm() {
+  const [error, setError] = useState("");
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: (formValue) => {
+      setError("");
+      const { username, password } = formValue;
+
+      if (username !== user.username || password !== user.password) {
+        setError("El usuario o la contraseña no son correctos");
+      } else {
+        console.log("Login correcto");
+        console.log(userDetails);
+      }
+    },
+  });
+
   return (
     <View>
       <Text style={styles.title}> Iniciar sesión </Text>
@@ -16,16 +38,39 @@ export default function LoginForm() {
         placeholder="Nombre de usuario"
         style={styles.input}
         autoCapitalize="none"
+        value={formik.values.username}
+        onChangeText={(text) => formik.setFieldValue("username", text)}
       ></TextInput>
       <TextInput
         placeholder="Contraseña"
         style={styles.input}
         autoCapitalize="none"
         secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue("password", text)}
       ></TextInput>
-      <Button title="Entrar" onPress={() => console.log("ENTRÓ")} />
+      <Button title="Entrar" onPress={formik.handleSubmit} />
+
+      <Text style={styles.error}>{formik.errors.username}</Text>
+      <Text style={styles.error}>{formik.errors.password}</Text>
+
+      <Text style={styles.error}>{error}</Text>
     </View>
   );
+}
+
+function initialValues() {
+  return {
+    username: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    username: Yup.string().required("El usuario es obligatorio"),
+    password: Yup.string().required("La contraseña es obligatoria"),
+  };
 }
 
 const styles = StyleSheet.create({
@@ -42,5 +87,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 10,
+  },
+  error: {
+    textAlign: "center",
+    color: "#f00",
+    // marginBottom: 20,
   },
 });
