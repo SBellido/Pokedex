e189bc6a52dde456c3e5743a3c93f1e48ebfda6c
SBellido <sebastianbellidodg@gmail.com>
2024-05-03 15:40:19 -0300

    UserData and LoginForm components was created

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { View, Text } from "react-native";
+import LoginForm from "../components/Auth/LoginForm";
+import UserData from "../components/Auth/UserData";
 
 export default function Account() {
+  const auth = null;
+
   return (
-    <SafeAreaView>
-      <Text>Estamos en MI CUENTA</Text>
-    </SafeAreaView>
+    <View>{auth ? <UserData /> : <LoginForm /> }</View>
   );
 }
---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = { 
+  username: "sbellido",
+  password: "27101979"
+}
+
+export const userDetails = {
+  username: "sbellido",
+  firstName: "Sebastian",
+  lastName: "Bellido Vega",
+  email: "sebastianbellidodg@gmail.com",
+}
